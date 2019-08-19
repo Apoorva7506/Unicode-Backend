69871c3abafbe4bbe5bfae5ab472751d0ee13eec
unknown <apoorvaflame@gmail.com>
2019-08-19 21:08:30 +0530

    task4 displaying data from database

--- spacex/dcollect/views.py
+++ spacex/dcollect/views.py
@@ -6,16 +6,10 @@ from django.http import HttpResponse,JsonResponse
 import dateutil.parser
 
 def index(request):
-    
-    t=Rdata.dataget()
-    for i in t:
 
     
-        p=Rdata(flight_number=i['flight_number'],rocket_name=i['rocket_name'],mission_patch_link=i['missionpatch'],date=i['date'])
-        p.save()
-     
+    
     y=Rdata.objects.all()
-
     return render(request,'index.html',{'yr':y })
     
     
